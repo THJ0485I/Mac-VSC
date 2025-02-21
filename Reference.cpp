61a661342c5a6067eb19590439ba7520aae238c6
HongJiang47_YT <94294806+THJ0485I@users.noreply.github.com>
2025-02-21 21:19:14 +0800

    Create Reference.cpp

---
+++ Reference.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string place = "Place";
+    cout << &place << endl;
+
+    // create a new reference for place variable
+    string& city = place;  
+
+    // Modify the referrence
+    // If we change the referrencec, we change the variable itself
+
+    city = "Sengkang";
+
+    cout << place << endl;
+
+
+    int height = 180;
+    int& height2 = height;
+    height2 = 200;
+    cout <, height << endl;
+
+    return 0;
+}
