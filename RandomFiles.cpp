d764ac37a25974a35e20386fa6c72c47d0fc1d9d
HongJiang47_YT <94294806+THJ0485I@users.noreply.github.com>
2025-02-21 21:22:01 +0800

    Create RandomFiles.cpp

---
+++ RandomFiles.cpp
@@ -0,0 +1,59 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    for (int i = 1; i <= 100; ++i) 
+    {
+        cout << "* - " << i << endl; // Print the value of i
+    }
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Build a function that reduces an original interger by 10
+void sum(n)
+{
+
+}
+
+int main()
+{
+    return 0;
+}
+
+///////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+int potion(int age) 
+{
+    return age - 10;
+}
+
+int main() 
+{
+    int age = 100;
+    age = potion(age);
+
+    cout << age << endl;
+
+    return 0;
+}
+
+
+string item = "dirt"
+
+void magic(string &item) 
+{
+    item = "diamond";
+}
