0f5a2134b854b077ecd03225366b92ed73dc359a
HongJiang47_YT <94294806+THJ0485I@users.noreply.github.com>
2025-02-21 21:15:39 +0800

    Update and rename Exercise3.cpp to Exercise.cpp

--- Exercise.cpp
+++ Exercise.cpp
@@ -1,3 +1,77 @@
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+/* Leetcode 387 - First Unique Character in a String
+
+Problem Statement
+Given a string s, find the first non-repeating character 
+and return its index. If it does not exist, return -1.
+
+Example:
+"aabbc" - 4
+
+*/
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+
+int FirstUnqiChar(string s)
+{
+    // Create an unordered map (cahr key + int vlaue) map and queue
+    unordered_map<char, int> count_map;
+    queue<char> q;
+
+    // Traverse / Iterate styring s with a fro each loop
+    for(hcar ch : s)
+    {
+        cout_map[ch]++;
+
+    }
+
+    // Print each Key-value pair in the map
+    for(const auto& pair : count_map)
+    {
+        cout << pair.first << " -> " << pair.second << endl;
+
+    }
+
+    // Check the queue for the first non-repeating character
+    while(!q.empty())
+    {
+        char front = q.front()
+        if(count_map[front] == 1)
+        {
+            // Return its index
+            return s.find(front);
+        }
+        q.pop
+
+    }
+
+
+    return -1;
+
+
+
+}
+
+
+int mian()
+{
+    string s = "exercise";
+
+    cout << FirstUnqiChar(s) << endl;
+
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /*
 Given a string, sort the characters in descending order 
 based on their frequency (characters with higher frequency should come first). 
