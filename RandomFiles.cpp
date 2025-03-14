a306ea619d8d90a01504ba0f29705760092ddbfc
HongJiang47_YT <94294806+THJ0485I@users.noreply.github.com>
2025-03-14 19:26:42 +0800

    Update RandomFiles.cpp

--- RandomFiles.cpp
+++ RandomFiles.cpp
@@ -57,3 +57,51 @@ void magic(string &item)
 {
     item = "diamond";
 }
+
+////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Pass by Value vs Pass by referrence
+// It has to do with functions
+
+// Modify an array by reference
+void Array_Multiplayer(int (&someArray)[10])
+{
+    // when there's &
+    // we mess with teh original data
+    for(int& i : someArray)
+    {
+        i = i * 2;
+    }
+}
+
+int main() 
+{
+    // Initilize an interger array
+    // Array size has to be declared in the` first place
+    // c++ array has fixed size
+    int weight[10] = {1, 2, 3, 4, 5};
+
+    for (int i : weight)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    Array_Multiplayer(weight);
+
+    for(int i : weight)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
+
+
