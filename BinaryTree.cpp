8cb35aeb00d60b5cb05bcb744f64ed792d62ee5e
HongJiang47_YT <94294806+THJ0485I@users.noreply.github.com>
2025-06-09 19:10:52 +0800

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -251,9 +251,43 @@ int main() {
     return 0;
 }
 
+//////////////////////
 
+/*
+Problem: Maximum Path Sum (Part 2)
 
+Print the path that leads to the maximum path sum in the binary tree
+*/
 
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) : data(val), left(NULL), right(NULL) {}
+};
+
+int main() {
+    /*
+            10
+           /  \
+          7    15
+         / \
+        3   9
+    */
+    Node* root = new Node(10);
+    root->left = new Node(7);
+    root->right = new Node(15);
+    root->left->left = new Node(3);
+    root->left->right = new Node(9);
+
+    return 0;
+}
 
 
 
